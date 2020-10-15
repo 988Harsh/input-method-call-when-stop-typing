74ccf5bc8e156785bffada997099b1a4baea9a24
988Harsh <harshp@argusoft.in>
2020-10-15 10:35:14 +0530

    input file task 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { debounceTime } from "rxjs/operators";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'input-call-function-after-typing';
+  islistening = true;
+  @ViewChild('methodTarget', { static: true }) paragraph: ElementRef;
+
+  methodToCall($event) {
+    if (this.islistening) {
+      this.islistening = false;
+      fromEvent($event.target, 'input')
+        .pipe(debounceTime(300))
+        .subscribe(event => this.someMethod(event));
+    }
+  }
+
+  someMethod($event) {
+    this.islistening = true;
+    this.paragraph.nativeElement.textContent = $event.target.value;
+  }
+
 }
